dacc6c5dff11e3801f9a105f0b8ed9fa1f2a4448
Ganesh Bhosale <ganesh@dwij.co.in>
2015-01-25 11:47:34 +0530

    Opponent cars added
    With overlap control
    Some bugs remaining

--- js/mygame.js
+++ js/mygame.js
@@ -14,6 +14,8 @@ var LANE_3_X = 309;
 var carLaneChangeSpeed = 200;
 var cntSpeed = 200;
 
+var allOppCars = [];
+
 // Creates a new 'main' state that will contain the game
 var mainState = {
 
@@ -24,7 +26,12 @@ var mainState = {
 
         // Load the bird sprite
         game.load.image('car', 'assets/car.png');
-
+        game.load.image('car1', 'assets/car1.png');
+        game.load.image('car2', 'assets/car2.png');
+        game.load.image('car3', 'assets/car3.png');
+        game.load.image('car4', 'assets/car4.png');
+        game.load.image('car5', 'assets/car5.png');
+        
         // Load the pipe sprite
         game.load.image('road', 'assets/road.png');
     },
@@ -37,7 +44,7 @@ var mainState = {
         // Display the bird on the screen
         this.car = this.game.add.sprite(LANE_2_X, 350, 'car');
         this.car.cntLane = LANE_2;
-
+        
         // Add gravity to the bird to make it fall
         game.physics.arcade.enable(this.car);
         //this.bird.body.gravity.y = 700;
@@ -50,6 +57,7 @@ var mainState = {
         this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(this.jumpDown, this);
         this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(this.jumpLeft, this);
         this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(this.jumpRight, this);
+        this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(this.jumpBreak, this);
 
         // Create a group of 20 pipes
         //this.pipes = game.add.group();
@@ -64,8 +72,12 @@ var mainState = {
         
         this.setSpeed(200);
         
+        this.background1.body.maxVelocity.y = 1000;
+        this.background2.body.maxVelocity.y = 1000;
+        
         this.car.bringToTop();
         
+        this.addOppsCars();
         //this.ground = new Ground(this.game, 0, 400, 335, 112);
 
         //this.addRowOfPipes();
@@ -132,23 +144,169 @@ var mainState = {
             }
         }
         
+        if(this.background1.body.acceleration.y != 0) {
+            if(this.background1.body.acceleration.y < 0 && this.background1.body.velocity.y <= 0) {
+                this.background1.body.acceleration.y = 0;
+                this.background2.body.acceleration.y = 0;
+                this.setSpeed(0);
+            } else if(this.background1.body.acceleration.y > 0 && this.background1.body.velocity.y >= 1000) {
+                this.background1.body.acceleration.y = 0;
+                this.background2.body.acceleration.y = 0;
+                this.setSpeed(1000);
+            }
+        }
+        
+        
+        
         // If the bird overlap any pipes, call 'restartGame'
         //game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);      
+        game.physics.arcade.overlap(this.car, this.car1, this.restartGame, null, this);
+        game.physics.arcade.overlap(this.car, this.car2, this.restartGame, null, this);
+        game.physics.arcade.overlap(this.car, this.car3, this.restartGame, null, this);
+        game.physics.arcade.overlap(this.car, this.car4, this.restartGame, null, this);
+        game.physics.arcade.overlap(this.car, this.car5, this.restartGame, null, this);
+        
+        this.checkOppCarOverlap();
+        
+    },
+    
+    checkOppCarOverlap: function() {
+        for(var i=0; i < this.allOppCars.length; i++) {
+            for(var j=0; j < this.allOppCars.length; j++) {
+                if(this.allOppCars[i].name != this.allOppCars[j].name) {
+                    if(game.physics.arcade.overlap(this.allOppCars[i], this.allOppCars[j], null, null, this)) {
+                        this.oppCarOverlap(this.allOppCars[i], this.allOppCars[j]);
+                    }
+                }
+            }
+        }
+    },
+    oppCarOverlap: function(car1, car2) {
+        console.log("----oppCarOverlap----"+car1.y+" : "+car2.y);
+        /*
+        if((car2.y+car2.height) < 0) {
+            this.setCarAgain(car2);
+            return;
+        } else if((car1.y+car1.height) < 0) {
+            this.setCarAgain(car1);
+            return;
+        }
+        */
+        if(car1.y > car2.y) {
+            car2.body.velocity.y = car1.body.velocity.y;
+            car2.y = (car1.y - car1.height - 0);
+        } else {
+            car1.body.velocity.y = car2.body.velocity.y;
+            car1.y = (car2.y - car2.height - 0);
+        }
+        console.log("----oppCarOverlap----"+car1.y+" : "+car2.y);
+    },
+    addOppsCars: function(speed) {
+        this.car1 = this.game.add.sprite(LANE_1_X, -350, 'car1');
+        this.car2 = this.game.add.sprite(LANE_3_X, -750, 'car2');
+        this.car3 = this.game.add.sprite(LANE_3_X, -250, 'car3');
+        this.car4 = this.game.add.sprite(LANE_2_X, -650, 'car4');
+        this.car5 = this.game.add.sprite(LANE_2_X, -950, 'car5');
+        
+        this.car1.name = "Car 1";
+        this.car2.name = "Car 2";
+        this.car3.name = "Car 3";
+        this.car4.name = "Car 4";
+        this.car5.name = "Car 5";
+        
+        game.physics.arcade.enable(this.car1);
+        game.physics.arcade.enable(this.car2);
+        game.physics.arcade.enable(this.car3);
+        game.physics.arcade.enable(this.car4);
+        game.physics.arcade.enable(this.car5);
+        
+        this.car1.body.velocity.y = 200;
+        this.car2.body.velocity.y = 300;
+        this.car3.body.velocity.y = 500;
+        this.car4.body.velocity.y = 400;
+        this.car5.body.velocity.y = 250;
+        
+        this.car1.checkWorldBounds = true;
+        this.car2.checkWorldBounds = true;
+        this.car3.checkWorldBounds = true;
+        this.car4.checkWorldBounds = true;
+        this.car5.checkWorldBounds = true;
+        
+        this.car1.events.onOutOfBounds.add(this.setCarAgain, this);
+        this.car2.events.onOutOfBounds.add(this.setCarAgain, this);
+        this.car3.events.onOutOfBounds.add(this.setCarAgain, this);
+        this.car4.events.onOutOfBounds.add(this.setCarAgain, this);
+        this.car5.events.onOutOfBounds.add(this.setCarAgain, this);
+        
+        this.allOppCars = [];
+        this.allOppCars.push(this.car1);
+        this.allOppCars.push(this.car2);
+        this.allOppCars.push(this.car3);
+        this.allOppCars.push(this.car4);
+        this.allOppCars.push(this.car5);
+    },
+    
+    setCarAgain: function(carObj) {
+        cx = parseInt(Math.random() * 3);
+        cy = parseInt(Math.random() * -2000);
+        
+        if(cx == 0) {
+            cx = LANE_1_X;
+        } else if(cx == 1) {
+            cx = LANE_2_X;
+        } else if(cx == 2) {
+            cx = LANE_3_X;
+        }
+        
+        console.log("setCarAgain: ("+cx+", "+cy+")");
+        carObj.x = cx;
+        carObj.y = cy;
+        //console.log(carObj);
+        this.checkOppCarOverlap();
+        carObj.bringToTop();
     },
     
     setSpeed: function(speed) {
         if(speed >= 0) {
-            console.log("setSpeed: from: "+this.cntSpeed+" to: "+speed);
-            this.cntSpeed = speed;
-            this.background1.body.velocity.y = this.cntSpeed;
-            this.background2.body.velocity.y = this.cntSpeed;
+            if(speed > this.background1.body.maxVelocity.y) {
+                console.log("setSpeed: from: "+this.cntSpeed+" to: "+this.background1.body.maxVelocity.y);
+                this.cntSpeed = this.background1.body.maxVelocity.y;
+                this.background1.body.velocity.y = this.background1.body.maxVelocity.y;
+                this.background2.body.velocity.y = this.background1.body.maxVelocity.y;
+            } else {
+                console.log("setSpeed: from: "+this.cntSpeed+" to: "+speed);
+                this.cntSpeed = speed;
+                this.background1.body.velocity.y = this.cntSpeed;
+                this.background2.body.velocity.y = this.cntSpeed;
+            }
         }
     },
+    setAcceleration : function(accl) {
+        //console.log("setAcceleration: set: "+accl);
+        if(accl < 0 && (-accl) > this.background1.body.velocity.y) {
+            accl = -this.background1.body.velocity.y;
+        } else if(accl > 0 && accl > this.background1.body.maxVelocity.y) {
+            accl = this.background1.body.maxVelocity.y;
+        }
+        //console.log("setAcceleration:  at: "+accl);
+        this.background1.body.acceleration.y = accl;
+        this.background2.body.acceleration.y = accl;
+    },
     
+    jumpBreak: function() {
+        if(this.background1.body.acceleration.y == 0) {
+            var accl = -100;
+            this.setAcceleration(accl);
+            this.cntSpeed = 0;
+        } else {
+            var accl = this.background1.body.acceleration.y - 100;
+            this.setAcceleration(accl);
+            this.cntSpeed = 0;
+        }
+    },
     jumpUp: function() {
         this.setSpeed(this.cntSpeed + 20);
     },
-    
     jumpDown: function() {
         this.setSpeed(this.cntSpeed - 20);
     },
@@ -182,6 +340,7 @@ var mainState = {
     // Restart the game
     restartGame: function() {
         // Start the 'main' state, which restarts the game
+        console.log("Game Restart !!!");
         game.state.start('main');
     },
 
